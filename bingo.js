ecf307c578bd4e91bbced1b0424a1ff350fec78b
The-Vishnu-Shahi <vishnushahi2108@gmail.com>
2024-09-08 13:26:33 +0530

    Add files via upload

---
+++ bingo.js
@@ -0,0 +1,15 @@
+const dev = {
+    fullName : "Dev Shahi" ,
+    age : 18,
+    hobbies : "Cricket" ,
+    passion : "naval" ,
+    isAchieve : true ,
+};
+let x = prompt("Enter Dev Number");
+if (x=== '420'){
+    console.log(dev);
+}
+
+else{
+    console.log("go fuck yourself");
+}
